d3609fd9bf708ca94c19b785a851cb06b8c177e1
dkolov84 <31516121+dkolov84@users.noreply.github.com>
2018-04-21 00:08:46 -0500

    fix js

--- js/accordion.js
+++ js/accordion.js
@@ -3,7 +3,11 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
         this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
@@ -11,4 +15,4 @@ for (i = 0; i < acc.length; i++) {
             panel.style.display = "block";
         }
     });
-}
+} 
