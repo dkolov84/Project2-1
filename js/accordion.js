54c44691b520785778b8c34e6c58fce00a9a11c3
dkolov84 <31516121+dkolov84@users.noreply.github.com>
2018-04-21 00:49:57 -0500

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -1,5 +1,14 @@
-     $(document).ready(function () {
-          $("#accordion").accordion({
-              active: false, collapsible: true, heightStyle: "content", alwaysOpen: false
-          });
-      });
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
